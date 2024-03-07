b0fbff5a2cbe3c485c37cb0063b2d33c1a8c2da4
hammadshafi <hammadshafi1122@gmail.com>
2024-03-07 13:54:58 -0800

    initial commit

---
+++ my-todo/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './components/Header';
+import Input from './components/Input';
+
+function App() {
+  return (
+    <>
+    <div className='2xl:px-96 sm:px-0'>
+
+    
+    <Header/>
+    <Input/>
+    </div>
+   
+    </>
+  );
+}
+
+export default App;
